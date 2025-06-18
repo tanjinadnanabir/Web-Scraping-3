dca977cd899dbe73692f21adc70de2361e6132ae
Tanjin Adnan Abir <tanjinadnanabir@gmail.com>
2025-06-18 19:15:36 +0600

    Updated shoplist

--- collection.py
+++ collection.py
@@ -10,14 +10,25 @@ options = Options()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--start-maximized")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+# options.add_argument("--lang=en-US")
+# prefs = {"intl.accept_languages": "en,en_US"}
+# options.add_experimental_option("prefs", prefs)
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.google.com/maps/search/laptop+shop+near+mirpur/@23.8139132,90.3332292,5684m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MDYxMS4wIKXMDSoASAFQAw%3D%3D")
 
 time.sleep(5)
 
-for _ in range(10):
-    driver.execute_script("document.querySelector('div[role=\"main\"] div[aria-label]').scrollBy(0, 1500)")
+# height = driver.execute_script('return document.body.scrollHeight')
+
+# print(height)
+
+# for i in range(0,height+1200,60):
+#     driver.execute_script(f'window.scrollTo(0,{i});')
+#     time.sleep(5)
+
+for _ in range(101):
+    driver.execute_script("document.querySelector('div[role=\"main\"] div[aria-label]').scrollBy(0, 5000)")
     time.sleep(5)
 
 listings = driver.find_elements(By.CSS_SELECTOR, '.Nv2PK')
@@ -30,12 +41,12 @@ for list in listings:
     try:
         shop_name = list.find_element(By.CSS_SELECTOR, '.qBF1Pd').text
     except:
-        shop_name = None
+        shop_name = 'N/A'
 
     try:
         phone_number = list.find_element(By.CSS_SELECTOR, '.UsdlK').text
     except:
-        phone_number = None
+        phone_number = 'N/A'
 
     shop_data.append({
         'Shop Name': shop_name,
